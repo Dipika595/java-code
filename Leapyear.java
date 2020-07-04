b3f93ee107498a6241631f58698c54aa3cfcf36d
Dipika595 <37621130+Dipika595@users.noreply.github.com>
2020-07-05 01:37:09 +0530

    modified

---
+++ Leapyear.java
@@ -0,0 +1,27 @@
+package ProgramsList;
+
+import java.util.Scanner;
+
+public class Leapyear {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int year=400;
+		/*
+		 * System.out.println("Enter the year: "); Scanner sc = new Scanner(System.in);
+		 * 
+		 * 
+		 * year=sc.nextInt();
+		 */
+		
+		if((year%400==0)||((year%4==0)&&(year%100!=0))){
+			System.out.println("Year " + year + " is a leap year");
+		}else
+		{
+			System.out.println("Year " + year + " is not a leap year");
+		}
+		
+	}
+
+}
